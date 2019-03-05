77366d54085d30f2324ecf5f4f16198bcd66d0c5
Roh Woohyeon <roh.woohyeon@gmail.com>
2019-03-05 19:27:31 +0900

    Install @types and set basic configuration

---
+++ .babelrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  preset: ['next/babel', '@zeit/next-typescript/babel']
+}
---
+++ next.config.js
@@ -0,0 +1,2 @@
+const withTypescript = require('@zeit/next-typescript/babel')
+module.exports = withTypescript()
