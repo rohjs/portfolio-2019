993c094dabf0dfa800089751c925eda40bbab07b
Roh Woohyeon <roh.woohyeon@gmail.com>
2019-03-11 19:57:44 +0900

    Implement a basic nextjs site with typescript support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript(
+module.exports = withTypescript({
   webpack(config, options) {
     return config
   }
-)
+})
