b44e67e98019573057170b1407c8be28b6ff0731
Roh Woohyeon <roh.woohyeon@gmail.com>
2019-03-11 20:26:42 +0900

    Update package.json and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
+const path = require('path')
 const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript({
-  webpack(config, options) {
-    return config
-  }
-})
+const withReactSvg = require('next-react-svg')
+const withCSS = require('@zeit/next-css')
+const withPlugins = require('next-compose-plugins')
+
+module.exports = withPlugins([
+  withTypescript,
+  [
+    withReactSvg,
+    {
+      include: path.resolve(__dirname, 'src/assets/images')
+    }
+  ],
+  [
+    withCSS,
+    {
+      cssModule: true
+    }
+  ]
+])
